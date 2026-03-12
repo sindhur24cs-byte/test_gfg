9b76fb03fcf1f95d93348f63d9f0b8a35a8ad46b
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-12 20:21:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    boolean twoSum(int arr[], int target) {
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] + arr[j] == target) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+}
