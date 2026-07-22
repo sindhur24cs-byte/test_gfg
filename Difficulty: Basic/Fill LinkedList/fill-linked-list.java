98d32b2cb9426af952894d2beb1cde3563a94003
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 20:02:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fill LinkedList/fill-linked-list.java
@@ -0,0 +1,9 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+class Solution {
+    // Function to fill all elements of the LinkedList with value k
+    public static void fillLinkedList(LinkedList<Integer> ll, int k) {
+        Collections.fill(ll, k);
+    }
+}
