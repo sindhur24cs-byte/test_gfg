35b8e19692c8aa8a1b997ea744da069c6d3b777d
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 16:57:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Max and Min In ArrayList/max-and-min-in-array-list.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    // Method to return the maximum element in the ArrayList
+    public int maximumElement(ArrayList<Integer> arr) {
+        return Collections.max(arr);
+    }
+
+    // Method to return the minimum element in the ArrayList
+    public int minimumElement(ArrayList<Integer> arr) {
+        return Collections.min(arr);
+    }
+}
