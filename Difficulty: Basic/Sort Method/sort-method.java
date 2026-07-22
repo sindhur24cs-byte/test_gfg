04344857bdaca653e4d6a9f660d35b8fa51ab83b
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 19:57:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort Method/sort-method.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    // Function to sort the array in increasing order
+    public static void sortArray(int arr[]) {
+        Arrays.sort(arr);
+    }
+}
