3070d82fc55bdeb201597aaff3f1a57536014729
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 16:33:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Factorial of BigInteger/factorial-of-big-integer.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class Solution {
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+}
