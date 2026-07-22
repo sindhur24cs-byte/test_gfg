6c89f853ac487113f15c965cc5abcfaaca8ea9b4
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 16:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multi Parameters Lambda Expression/multi-parameters-lambda-expression.java
@@ -0,0 +1,6 @@
+class Solution {
+    public Add helperFunction() {
+        // Implement addParameters(int a, int b) using a multi-parameter lambda expression
+        return (a, b) -> a + b;
+    }
+}
