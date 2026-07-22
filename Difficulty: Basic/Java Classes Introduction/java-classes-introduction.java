4f2abbb7020008aed058056b2c8093b3b2d3ac2e
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 16:13:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Classes Introduction/java-classes-introduction.java
@@ -0,0 +1,22 @@
+class task {
+    private int length;
+    private int width;
+    private int height;
+
+    public void set_length(int l) {
+        this.length = l;
+    }
+
+    public void set_width(int w) {
+        this.width = w;
+    }
+
+    public void set_height(int h) {
+        this.height = h;
+    }
+
+    public void volume() {
+        long vol = (long) length * width * height;
+        System.out.println(vol);
+    }
+}
