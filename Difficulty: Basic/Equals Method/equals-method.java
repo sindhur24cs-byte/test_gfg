d1212be4409a77fbe23781434c39c8b6467a766d
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-22 19:56:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Equals Method/equals-method.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    // Function to check if two arrays contain the same elements in the same order
+    public static boolean areEqual(int a[], int b[]) {
+        return Arrays.equals(a, b);
+    }
+}
