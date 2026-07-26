30dbdf7253a9f7a35e13d742253d758005771086
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-26 15:39:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int celebrity(int mat[][]) {
+        int n = mat.length;
+
+        int candidate = 0;
+
+       
+        for (int i = 1; i < n; i++) {
+            if (mat[candidate][i] == 1) {
+                candidate = i;
+            }
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            if (i != candidate) {
+              
+                if (mat[candidate][i] == 1 || mat[i][candidate] == 0) {
+                    return -1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
