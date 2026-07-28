da57f6024bb93df1f1f0cd87e4501094f0231dea
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-28 19:06:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Transition Point/find-transition-point.java
@@ -0,0 +1,20 @@
+class Solution {
+    int transitionPoint(int arr[]) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == 1) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
