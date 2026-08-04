6a050b98fd6418d1bfdcb7c50b18aeb29dfddb08
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-08-04 17:33:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non-Repeating Element/non-repeating-element.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int firstNonRepeating(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : arr) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+
+        return 0;
+    }
+}
