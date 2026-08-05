57e2b2726f9a8d14d1f511e83b8976116e481767
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-08-05 16:02:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Subarrays with Sum in Range/subarrays-with-sum-in-range.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    long countAtMost(int[] arr, int k) {
+        if (k < 0) return 0;
+
+        int left = 0;
+        long sum = 0;
+        long count = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+
+            while (sum > k) {
+                sum -= arr[left++];
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    public int countSubarray(int[] arr, int l, int r) {
+        return (int)(countAtMost(arr, r) - countAtMost(arr, l - 1));
+    }
+}
