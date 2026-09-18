28eee99f5a0ae4972e375e86ae6331e23c1a3be7
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-09-18 22:05:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Absolute Difference In BST/minimum-absolute-difference-in-bst.java
@@ -0,0 +1,23 @@
+class Solution {
+    int prev = -1;
+    int ans = Integer.MAX_VALUE;
+
+    void inorder(Node root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+
+        if (prev != -1)
+            ans = Math.min(ans, root.data - prev);
+
+        prev = root.data;
+
+        inorder(root.right);
+    }
+
+    int absDiff(Node root) {
+        inorder(root);
+        return ans;
+    }
+}
